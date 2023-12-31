ea188dd284f99faf981e131073bb0a0acfc5a02e
Tareq1131 <tareqislam076@gmail.com>
2024-01-01 02:06:51 +0600

    nested object and Arrays

--- src/Components/YoutubeFrom.js
+++ src/Components/YoutubeFrom.js
@@ -3,17 +3,17 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import TextError from "./TextError";
 
-const initialValues = { 
-    name: "", 
-email: "", 
-channel: "",
-comments: "",
-address: "",
-social: {
-    facebook: '',
-    twitter: ''
-}
-
+const initialValues = {
+  name: "",
+  email: "",
+  channel: "",
+  comments: "",
+  address: "",
+  social: {
+    facebook: "",
+    twitter: "",
+  },
+  phoneNumber: ["", ""],
 };
 
 const onSubmit = (values) => {
@@ -39,59 +39,67 @@ function YoutubeFrom() {
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <Field type="text" id="name" name="name" />
-          <ErrorMessage name="name" component ={TextError}/>
+          <ErrorMessage name="name" component={TextError} />
         </div>
 
         <div className="form-control">
           <label htmlFor="email">E-mail</label>
           <Field type="email" id="email" name="email" />
           <ErrorMessage name="email">
-            {
-                (errorMsg)=><div className="error">{errorMsg}</div>
-
-                
-            }
+            {(errorMsg) => <div className="error">{errorMsg}</div>}
           </ErrorMessage>
         </div>
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <Field type="text" id="channel" name="channel" placeholder= 'Youtube channel Name'/>
-          <ErrorMessage name="channel" component ={TextError}/>
+          <Field
+            type="text"
+            id="channel"
+            name="channel"
+            placeholder="Youtube channel Name"
+          />
+          <ErrorMessage name="channel" component={TextError} />
         </div>
 
         <div className="form-control">
-        <label htmlFor="comments">Comments</label>
-        <Field as='textarea' id='comments' name='comments'/>
-
+          <label htmlFor="comments">Comments</label>
+          <Field as="textarea" id="comments" name="comments" />
         </div>
         <div className="form-control">
-        <label htmlFor="address">Address</label>
-        <Field name='address'>
-        {
-            props=>{
-                const {field,form,meta}= props
-                console.log('Render props',props)
-                return (<div> 
-                    <input type='text'id='address'{...field}></input> 
-                    {meta.touched && meta.error ? <div>{meta.error}</div> : null}
-                    </div>
-                    
-                    )
-            }
-        }
-        
-        </Field>
+          <label htmlFor="address">Address</label>
+          <Field name="address">
+            {(props) => {
+              const { field, form, meta } = props;
+              console.log("Render props", props);
+              return (
+                <div>
+                  <input type="text" id="address" {...field}></input>
+                  {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                </div>
+              );
+            }}
+          </Field>
         </div>
 
         <div className="form-control">
-        <label htmlFor="facebook">Facebook profile</label>
-        <Field type='text' id='facebook' name= 'social.facebook'/>
+          <label htmlFor="facebook">Facebook profile</label>
+          <Field type="text" id="facebook" name="social.facebook" />
         </div>
 
         <div className="form-control">
-        <label htmlFor="twitter">Twitter profile</label>
-        <Field type='text' id='twitter' name= 'social.twitter'/>
+          <label htmlFor="twitter">Twitter profile</label>
+          <Field type="text" id="twitter" name="social.twitter" />
         </div>
+
+        <div className="form-control">
+          <label htmlFor="primaryPh">Primary phone number</label>
+          <Field type="text" id="primaryPh" name="phoneNumbers[0]" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="secondaryPh">Secondary phone number</label>
+          <Field type="text" id="secondaryPh" name="phoneNumbers[1]" />
+        </div>
+
         <button type="submit">Submit</button>
       </Form>
     </Formik>

075fcb0029cafcdf7ba9244322c4635746bb205a
Tareq1131 <tareqislam076@gmail.com>
2023-12-31 14:00:27 +0600

    Schema validation with Yup

--- src/Components/YoutubeFrom.js
+++ src/Components/YoutubeFrom.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Formik, useFormik } from "formik";
+import * as Yup from 'yup'
 
 const initialValues = { name: "", email: "", channel: "" };
 
@@ -7,33 +8,43 @@ const onSubmit = (values) => {
   console.log("form Data", values);
 };
 
-const validate = (values) => {
-  //values.name values.email,values.channel
-  //errors.name errors.email,errors.channel
-  //errors.name ='this field is required'
+// const validate = (values) => {
+//   //values.name values.email,values.channel
+//   //errors.name errors.email,errors.channel
+//   //errors.name ='this field is required'
 
-  let errors = {};
-  if (!values.name) {
-    errors.name = "Required";
-  }
+//   let errors = {};
+//   if (!values.name) {
+//     errors.name = "Required";
+//   }
 
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email format";
-  }
-  if (!values.channel) {
-    errors.channel = "Required";
-  }
+//   if (!values.email) {
+//     errors.email = "Required";
+//   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+//     errors.email = "Invalid email format";
+//   }
+//   if (!values.channel) {
+//     errors.channel = "Required";
+//   }
 
-  return errors;
-};
+//   return errors;
+// };
+
+//Yup
+const validationSchema = Yup.object({
+    name: Yup.string().required('Required'),
+    email: Yup.string()
+      .email('Invalid email format')
+      .required('Required'),
+    channel: Yup.string().required('Required')
+  })
 
 function YoutubeFrom() {
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validate,
+    // validate,
+    validationSchema
   });
 
   //   console.log("Form values", formik.values);

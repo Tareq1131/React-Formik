3f55f2539a1058faaad73d9121f7af67df921695
Tareq1131 <tareqislam076@gmail.com>
2024-01-06 20:12:16 +0600

    Disabling submit

--- src/Components/YoutubeFrom.js
+++ src/Components/YoutubeFrom.js
@@ -44,110 +44,119 @@ function YoutubeFrom() {
       validationSchema={validationSchema}
       onSubmit={onSubmit}
     >
-      <Form>
-        <div className="form-control">
-          <label htmlFor="name">Name</label>
-          <Field type="text" id="name" name="name" />
-          <ErrorMessage name="name" component={TextError} />
-        </div>
+      {(formik) => {
+        console.log("Formik.props",formik)
+        return(
+        <Form>
+          <div className="form-control">
+            <label htmlFor="name">Name</label>
+            <Field type="text" id="name" name="name" />
+            <ErrorMessage name="name" component={TextError} />
+          </div>
 
-        <div className="form-control">
-          <label htmlFor="email">E-mail</label>
-          <Field type="email" id="email" name="email" />
-          <ErrorMessage name="email">
-            {(errorMsg) => <div className="error">{errorMsg}</div>}
-          </ErrorMessage>
-        </div>
-        <div className="form-control">
-          <label htmlFor="channel">Channel</label>
-          <Field
-            type="text"
-            id="channel"
-            name="channel"
-            placeholder="Youtube channel Name"
-          />
-          <ErrorMessage name="channel" component={TextError} />
-        </div>
+          <div className="form-control">
+            <label htmlFor="email">E-mail</label>
+            <Field type="email" id="email" name="email" />
+            <ErrorMessage name="email">
+              {(errorMsg) => <div className="error">{errorMsg}</div>}
+            </ErrorMessage>
+          </div>
+          <div className="form-control">
+            <label htmlFor="channel">Channel</label>
+            <Field
+              type="text"
+              id="channel"
+              name="channel"
+              placeholder="Youtube channel Name"
+            />
+            <ErrorMessage name="channel" component={TextError} />
+          </div>
 
-        <div className="form-control">
-          <label htmlFor="comments">Comments</label>
-          <Field
-            as="textarea"
-            id="comments"
-            name="comments"
-            validate={validateComments}
-          />
-          <ErrorMessage name="comments" component={TextError} />
-        </div>
+          <div className="form-control">
+            <label htmlFor="comments">Comments</label>
+            <Field
+              as="textarea"
+              id="comments"
+              name="comments"
+              validate={validateComments}
+            />
+            <ErrorMessage name="comments" component={TextError} />
+          </div>
 
-        <div className="form-control">
-          <label htmlFor="address">Address</label>
-          <Field name="address">
-            {(props) => {
-              const { field, form, meta } = props;
-              console.log("Render props", props);
-              return (
-                <div>
-                  <input type="text" id="address" {...field}></input>
-                  {meta.touched && meta.error ? <div>{meta.error}</div> : null}
-                </div>
-              );
-            }}
-          </Field>
-        </div>
+          <div className="form-control">
+            <label htmlFor="address">Address</label>
+            <Field name="address">
+              {(props) => {
+                const { field, form, meta } = props;
+                console.log("Render props", props);
+                return (
+                  <div>
+                    <input type="text" id="address" {...field}></input>
+                    {meta.touched && meta.error ? (
+                      <div>{meta.error}</div>
+                    ) : null}
+                  </div>
+                );
+              }}
+            </Field>
+          </div>
 
-        <div className="form-control">
-          <label htmlFor="facebook">Facebook profile</label>
-          <Field type="text" id="facebook" name="social.facebook" />
-        </div>
+          <div className="form-control">
+            <label htmlFor="facebook">Facebook profile</label>
+            <Field type="text" id="facebook" name="social.facebook" />
+          </div>
 
-        <div className="form-control">
-          <label htmlFor="twitter">Twitter profile</label>
-          <Field type="text" id="twitter" name="social.twitter" />
-        </div>
+          <div className="form-control">
+            <label htmlFor="twitter">Twitter profile</label>
+            <Field type="text" id="twitter" name="social.twitter" />
+          </div>
 
-        <div className="form-control">
-          <label htmlFor="primaryPh">Primary phone number</label>
-          <Field type="text" id="primaryPh" name="phoneNumbers[0]" />
-        </div>
+          <div className="form-control">
+            <label htmlFor="primaryPh">Primary phone number</label>
+            <Field type="text" id="primaryPh" name="phoneNumbers[0]" />
+          </div>
 
-        <div className="form-control">
-          <label htmlFor="secondaryPh">Secondary phone number</label>
-          <Field type="text" id="secondaryPh" name="phoneNumbers[1]" />
-        </div>
+          <div className="form-control">
+            <label htmlFor="secondaryPh">Secondary phone number</label>
+            <Field type="text" id="secondaryPh" name="phoneNumbers[1]" />
+          </div>
 
-        {/* FieldArray component for phone list */}
-        <div className="form-control">
-          <label>List of phone numbers</label>
-          <FieldArray name="phNumbers">
-            {(fieldArrayProps) => {
-              const { push, remove, form } = fieldArrayProps;
-              const { values } = form;
-              const { phNumbers } = values;
-              // console.log('fieldArrayProps', fieldArrayProps)
-              // console.log('Form errors', form.errors)
-              return (
-                <div>
-                  {phNumbers.map((phNumber, index) => (
-                    <div key={index}>
-                      <Field name={`phNumbers[${index}]`} />
-                      {index > 0 && (
-                        <button type="button" onClick={() => remove(index)}>
-                          -
-                        </button>
-                      )}
-                    </div>
-                  ))}
-                  <button type="button" onClick={() => push("")}>
-                    +
-                  </button>
-                </div>
-              );
-            }}
-          </FieldArray>
-        </div>
-        <button type="submit">Submit</button>
-      </Form>
+          {/* FieldArray component for phone list */}
+          <div className="form-control">
+            <label>List of phone numbers</label>
+            <FieldArray name="phNumbers">
+              {(fieldArrayProps) => {
+                const { push, remove, form } = fieldArrayProps;
+                const { values } = form;
+                const { phNumbers } = values;
+                // console.log('fieldArrayProps', fieldArrayProps)
+                // console.log('Form errors', form.errors)
+                return (
+                  <div>
+                    {phNumbers.map((phNumber, index) => (
+                      <div key={index}>
+                        <Field name={`phNumbers[${index}]`} />
+                        {index > 0 && (
+                          <button type="button" onClick={() => remove(index)}>
+                            -
+                          </button>
+                        )}
+                      </div>
+                    ))}
+                    <button type="button" onClick={() => push("")}>
+                      +
+                    </button>
+                  </div>
+                );
+              }}
+            </FieldArray>
+          </div>
+          <button className="btn" type="submit" disabled={!formik.isValid}>
+            Submit
+          </button>
+        </Form>
+        )
+      }}
     </Formik>
   );
 }
